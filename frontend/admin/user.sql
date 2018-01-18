e614e1c9c2f81639702ce7c221d335d4a724353c
yopekg <farizonadio@gmail.com>
2018-01-18 04:29:03 -0500

    Update user.sql

--- frontend/admin/user.sql
+++ frontend/admin/user.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.0
+-- version 4.7.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 11, 2017 at 10:08 AM
+-- Generation Time: Jan 18, 2018 at 08:51 AM
 -- Server version: 10.1.26-MariaDB
--- PHP Version: 7.1.8
+-- PHP Version: 7.1.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -31,6 +31,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `user` (
   `id` int(11) NOT NULL,
   `name` varchar(225) NOT NULL,
+  `email` varchar(30) NOT NULL,
   `username` varchar(225) NOT NULL,
   `password` varchar(225) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -39,8 +40,8 @@ CREATE TABLE `user` (
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id`, `name`, `username`, `password`) VALUES
-(1, 'admin', 'admin', 'adm!n!23');
+INSERT INTO `user` (`id`, `name`, `email`, `username`, `password`) VALUES
+(1, 'admin', 'adminadminbengkelsam@mailinato', 'admin', 'adm!n!23');
 
 --
 -- Indexes for dumped tables
@@ -51,6 +52,16 @@ INSERT INTO `user` (`id`, `name`, `username`, `password`) VALUES
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
